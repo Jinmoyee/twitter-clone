6a07a477337bb2f9a35d5a571b0a6632ee0ff280
Jinmoyee Thakuria <jinmoyee99@gmail.com>
2024-08-08 00:51:25 +0530

    minor setup

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js"
+import dotenv from "dotenv"
+import connectMongoDb from "./db/mongoDbConnect.js"
 
+dotenv.config();
 const app = express();
-
-app.get("/",(req,res) => {
-    res.send("Hello World!");
-})
 
 app.use("/api/auth", authRoutes)
 
 app.listen("1000", () => {
     console.log("Server is running on port 1000");
+    connectMongoDb();
 })
