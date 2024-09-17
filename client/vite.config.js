4ad263d42bd12a79f9e3823f982e9599731750e8
Jinmoyee Thakuria <jinmoyee99@gmail.com>
2024-09-18 03:59:17 +0530

    some change

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:1000',
+        //target: 'http://localhost:1000',
+        target: "https://twitter-clone-n54h.onrender.com/",
         changeOrigin: true,
       },
     },
