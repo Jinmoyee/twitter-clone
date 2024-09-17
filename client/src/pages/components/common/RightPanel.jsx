a3389e4b71c6f374f19f09387fa4586d9afd3da1
Jinmoyee Thakuria <jinmoyee99@gmail.com>
2024-09-18 02:26:05 +0530

    all done

--- client/src/pages/components/common/RightPanel.jsx
+++ client/src/pages/components/common/RightPanel.jsx
@@ -1,80 +1,21 @@
-// import { useState } from 'react'
-// import { USERS_FOR_RIGHT_PANEL } from '../../../utils/db/dummey'
-import RightPanelSkeleton from '../skeletons/RightPanelSkeleton'
-import { useQuery } from '@tanstack/react-query'
-import { Link } from 'react-router-dom'
-import useFollow from '../../hooks/useFollow'
-import LoadingSpinner from './LoadingSpinner'
+import { TwitterTimelineEmbed, TwitterTweetEmbed } from "react-twitter-embed";
+import { FaSearch } from "react-icons/fa";
 export default function RightPanel() {
-    const { data: suggestedUsers, isLoading } = useQuery({
-        queryKey: ['suggestedUsers'],
-        queryFn: async () => {
-            // Fetch data from API
-            try {
-                const response = await fetch('/api/users/suggested')
-                const data = await response.json()
-                if (!response.ok) {
-                    throw new Error(data.error || 'Failed to fetch suggested users')
-                }
-                return data
-            } catch (err) {
-                throw new Error(err.message)
-            }
-        },
-    })
-
-    const { follow } = useFollow()
-
-    if (suggestedUsers?.length === 0) return <div className='md:w-64 w-0'></div>;
 
     return (
-        <div className='hidden lg:block my-4 mx-2'>
-            <div className='p-4 rounded-md sticky top-2 border-[0.15rem] border-neutral'>
-                <p className='font-bold mb-1 text-lg'>Who to follow</p>
-                <div className='flex flex-col gap-4'>
-                    {/* item */}
-                    {isLoading && (
-                        <>
-                            <RightPanelSkeleton />
-                            <RightPanelSkeleton />
-                            <RightPanelSkeleton />
-                            <RightPanelSkeleton />
-                        </>
-                    )}
-                    {!isLoading &&
-                        suggestedUsers?.map((user) => (
-                            <Link
-                                to={`/profile/${user.username}`}
-                                className='flex items-center justify-between gap-4'
-                                key={user._id}
-                            >
-                                <div className='flex gap-2 items-center'>
-                                    <div className='avatar'>
-                                        <div className='w-10 rounded-full'>
-                                            <img src={user.profileImg || "/avatar-placeholder.png"} />
-                                        </div>
-                                    </div>
-                                    <div className='flex flex-col'>
-                                        <span className='font-semibold tracking-tight truncate w-28'>
-                                            {user.fullName}
-                                        </span>
-                                        <span className='text-sm text-slate-500'>@{user.username}</span>
-                                    </div>
-                                </div>
-                                <div>
-                                    <button
-                                        className='btn text-neutral border border-neutral hover:bg-amber-950 hover:text-white hover:opacity-90 rounded-full btn-sm'
-                                        onClick={(e) => {
-                                            e.preventDefault();
-                                            follow(user._id);
-                                        }}
-                                    >
-                                        Follow
-                                    </button>
-                                </div>
-                            </Link>
-                        ))}
-                </div>
+        <div className="p-3 hidden md:inline">
+            <div className="flex items-center relative">
+                <FaSearch className="text-xl absolute top-3 right-3" />
+                <input placeholder="Search Twitter" type="text" className="px-3 py-2 rounded-full border-2 w-full" />
+            </div>
+            <div className="mt-2 bg-slate-200 p-3 rounded-2xl">
+                <h2 className="text-xl font-bold">What's Happening</h2>
+                <TwitterTweetEmbed tweetId={"1816174440071241866"} />
+                <TwitterTimelineEmbed
+                    sourceType="profile"
+                    screenName="Valorant"
+                    options={{ height: 400 }}
+                />
             </div>
         </div>
     )
