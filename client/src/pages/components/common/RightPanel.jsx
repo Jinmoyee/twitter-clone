9815b6d961c6387cd3c80ff29fe62adbdf7ba957
Jinmoyee Thakuria <jinmoyee99@gmail.com>
2024-09-19 03:51:13 +0530

    test

--- client/src/pages/components/common/RightPanel.jsx
+++ client/src/pages/components/common/RightPanel.jsx
@@ -1,10 +1,10 @@
-import { TwitterTimelineEmbed, TwitterTweetEmbed } from "react-twitter-embed";
-import { FaSearch } from "react-icons/fa";
+// import { TwitterTimelineEmbed, TwitterTweetEmbed } from "react-twitter-embed";
+// import { FaSearch } from "react-icons/fa";
 export default function RightPanel() {
 
     return (
         <div className="p-3 hidden md:inline">
-            <div className="flex items-center relative">
+            {/* <div className="flex items-center relative">
                 <FaSearch className="text-xl absolute top-3 right-3" />
                 <input placeholder="Search Twitter" type="text" className="px-3 py-2 rounded-full border-2 w-full" />
             </div>
@@ -16,7 +16,7 @@ export default function RightPanel() {
                     screenName="Valorant"
                     options={{ height: 400 }}
                 />
-            </div>
+            </div> */}
         </div>
     )
 }
