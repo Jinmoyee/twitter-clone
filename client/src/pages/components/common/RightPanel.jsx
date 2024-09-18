3eb37584d8c1e5398f8498fa82dd7926b6b2d53b
Jinmoyee Thakuria <jinmoyee99@gmail.com>
2024-09-19 03:58:59 +0530

    widget problem fixing

--- client/src/pages/components/common/RightPanel.jsx
+++ client/src/pages/components/common/RightPanel.jsx
@@ -1,22 +1,34 @@
-// import { TwitterTimelineEmbed, TwitterTweetEmbed } from "react-twitter-embed";
-// import { FaSearch } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
+import React, { useEffect } from 'react';
+
 export default function RightPanel() {
+    useEffect(() => {
+        // Load Twitter script dynamically
+        const script = document.createElement('script');
+        script.src = 'https://platform.twitter.com/widgets.js';
+        script.async = true;
+        document.body.appendChild(script);
+    }, []);
 
     return (
         <div className="p-3 hidden md:inline">
-            {/* <div className="flex items-center relative">
+            <div className="flex items-center relative">
                 <FaSearch className="text-xl absolute top-3 right-3" />
                 <input placeholder="Search Twitter" type="text" className="px-3 py-2 rounded-full border-2 w-full" />
             </div>
             <div className="mt-2 bg-slate-200 p-3 rounded-2xl">
                 <h2 className="text-xl font-bold">What's Happening</h2>
-                <TwitterTweetEmbed tweetId={"1816174440071241866"} />
-                <TwitterTimelineEmbed
-                    sourceType="profile"
-                    screenName="Valorant"
-                    options={{ height: 400 }}
-                />
-            </div> */}
+                <blockquote className="twitter-tweet">
+                    <a href="https://twitter.com/Twitter/status/1816174440071241866">Tweet</a>
+                </blockquote>
+                <a
+                    className="twitter-timeline"
+                    href="https://twitter.com/Valorant"
+                    data-height="400"
+                >
+                    Tweets by Valorant
+                </a>
+            </div>
         </div>
-    )
+    );
 }
